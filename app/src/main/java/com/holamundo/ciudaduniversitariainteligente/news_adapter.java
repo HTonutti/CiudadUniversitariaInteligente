f9887cddb80e93872813f5b79d0ab078b05aebf2
sfenoglio <safenoglio@gmail.com>
2019-06-11 15:40:01 -0300

    Agregado Novedades y Menú del día

---
+++ app/src/main/java/com/holamundo/ciudaduniversitariainteligente/news_adapter.java
@@ -0,0 +1,6 @@
+package com.holamundo.ciudaduniversitariainteligente;
+
+import android.widget.BaseAdapter;
+
+public class news_adapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/holamundo/ciudaduniversitariainteligente/news_fragment.java
@@ -0,0 +1,6 @@
+package com.holamundo.ciudaduniversitariainteligente;
+
+import android.app.Fragment;
+
+public class news_fragment extends Fragment {
+}
